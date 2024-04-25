0706cd53a7ad91b92d5b738f7b73b7f2db6a8e75
Leifeng2000 <leifeng7112@gmail.com>
2024-04-25 19:25:14 +0700

    u

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string sourceFolder = @"D:\New folder (7)\Data"; //File nguồn
+        string sourceFilePath = @"D:\New folder (7)\Data\Source.txt";
+        
+        //Directory.CreateDirectory(sourceFolder);
+        //File.CreateText(sourceFilePath);
+        ///
+
+        DirectoryInfo directoryInfo= new DirectoryInfo(sourceFolder);
+        directoryInfo.Create();
+
+        FileInfo fileInfo = new FileInfo(sourceFilePath);
+        fileInfo.Create();
+        
+    }
+}
